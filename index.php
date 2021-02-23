e1a9d11975ace71707e89950215dcc0a72b30933
Vladimir Kalachikhin <v.kalachikhin@gmail.com>
2021-02-23 12:07:19 +0300

    bug fixed

--- index.php
+++ index.php
@@ -8,13 +8,14 @@ require('params.php'); 	//
 //echo $_SERVER['PHP_SELF'];
 clearstatcache(TRUE,$selfStatusFileName); 	// from params.php
 if((time() - @filemtime($selfStatusFileName)) > $selfStatusTimeOut) $status = array(); 	// статус протух
-else $status = unserialize(file_get_contents($selfStatusFileName)); 	// считаем файл состояния
+else $status = unserialize(@file_get_contents($selfStatusFileName)); 	// считаем файл состояния
 if(!$status) {
 	$status = array();
 	$status[0]=15; 	// Navigational status 0 = under way using engine, 1 = at anchor, 2 = not under command, 3 = restricted maneuverability, 4 = constrained by her draught, 5 = moored, 6 = aground, 7 = engaged in fishing, 8 = under way sailing, 9 = reserved for future amendment of navigational status for ships carrying DG, HS, or MP, or IMO hazard or pollutant category C, high speed craft (HSC), 10 = reserved for future amendment of navigational status for ships carrying dangerous goods (DG), harmful substances (HS) or marine pollutants (MP), or IMO hazard or pollutant category A, wing in ground (WIG);11 = power-driven vessel towing astern (regional use), 12 = power-driven vessel pushing ahead or towing alongside (regional use); 13 = reserved for future use, 14 = AIS-SART (active), MOB-AIS, EPIRB-AIS 15 = undefined = default (also used by AIS-SART, MOB-AIS and EPIRB-AIS under test)
 	$status[1]='';
 	$_REQUEST['statusUpdated'] = 1;
 }
+//echo "_REQUEST <pre>";print_r($_REQUEST);echo "</pre><br>\n";
 //echo "status <pre>";print_r($status);echo "</pre><br>\n";
 
 // Сервер
@@ -126,9 +127,9 @@ elseif($_REQUEST['startClient']) { 	//
 	}
 }
 // изменение статуса
-elseif($_REQUEST['vehacleStatus'] or $_REQUEST['vehicleDescription']) { 	// 
+elseif($_REQUEST['vehacleStatus'] or $_REQUEST['vehicleDescription'] or ($_REQUEST['vehacleStatus']=='0')) { 	// 
 	//echo "vehacleStatus={$_REQUEST['vehacleStatus']}; vehicleDescription={$_REQUEST['vehicleDescription']};<br>\n";
-	$status[0]=$_REQUEST['vehacleStatus']; 	// 
+	$status[0]=(int)$_REQUEST['vehacleStatus']; 	// 
 	$status[1]=$_REQUEST['vehicleDescription'];
 }
 
@@ -281,13 +282,14 @@ else {
 </tr></table>
 </form>
 </div>
+<?php ?>
 <form  action='<?php echo $_SERVER['PHP_SELF'];?>' id='status'
 style='margin:0.5rem 0 0.5rem 0;padding:0.5rem;border:1px solid black;border-radius:5px;'
 >
 <!--0 = under way using engine, 1 = at anchor, 2 = not under command, 3 = restricted maneuverability, 4 = constrained by her draught, 5 = moored, 6 = aground, 7 = engaged in fishing, 8 = under way sailing, 11 = power-driven vessel towing astern (regional use), 12 = power-driven vessel pushing ahead or towing alongside (regional use);  15 = undefined = default -->
 <!--0 = Двигаюсь под мотором, 1 = На якоре, 2 = Без экипажа, 3 = Ограничен в манёвре, 4 = Ограничен осадкой, 5 = Ошвартован, 6 = На мели, 7 = Занят ловлей рыбы, 8 = Двигаюсь под парусом, 11 = Тяну буксир (regional use), 12 = Толкаю состав или буксирую под бортом (regional use);  15 = неопределённое = default -->
 	<select name='vehacleStatus' onchange="this.form.submit()" size='1' style='width:100%;font-size:150%;text-align: center;'>
-		<option value='0' <?php if($status[0] === 0) echo "selected=1";?> ><?php echo $AISstatusTXT[0]; ?></option>
+		<option value='0' <?php if($status[0] == 0) echo "selected=1";?> ><?php echo $AISstatusTXT[0]; ?></option>
 		<option value='1' <?php if($status[0] == 1) echo "selected=1";?> ><?php echo $AISstatusTXT[1]; ?></option>
 		<option value='2' <?php if($status[0] == 2) echo "selected=1";?> ><?php echo $AISstatusTXT[2]; ?></option>
 		<option value='3' <?php if($status[0] == 3) echo "selected=1";?> ><?php echo $AISstatusTXT[3]; ?></option>
